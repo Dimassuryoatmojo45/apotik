d9366479d9552da572f23a9479414b3a37437435
Dimas Suryo A <107175975+Dimassuryoatmojo45@users.noreply.github.com>
2025-03-06 15:01:31 +0700

    Login Register Api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Actions\Fortify\PasswordValidationRules;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Validate incoming request
+        $validated = $request->validate([
+            'username' => ['required', function($attribute, $value, $fail) {
+                if (empty($value)) {
+                    $fail('The ' . $attribute . ' field is required.');
+                }
+            }],
+            'password' => 'required|min:3',
+        ]);
+
+        try {
+            // Attempt to authenticate the user
+            if (Auth::attempt(['username' => $request->username, 'password' => $request->password])) {
+                $user = Auth::user();
+                $token = $user->createToken('MyApp')->plainTextToken;
+    
+                return response()->json([
+                    'token' => $token,
+                    'user' => $user,
+                ]);
+            } else {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+        } catch (\Exception $e) {
+            // Log exception and return error response
+            \Log::error('Login Error: ' . $e->getMessage());
+            return response()->json(['message' => 'Internal Server Error'], 500);
+        }
+    }
+
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function register(Request $request)
+    {
+        $input = $request->all();
+
+        $a = Validator::make($input, [
+            'username' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => $this->passwordRules(),
+            // 'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        // Create a new user
+        $user = User::create([
+            'username' => $input['username'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]);
+
+        // Generate API token for the user
+        $token = $user->createToken('YourAppName')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ]);
+
+    }
+}
