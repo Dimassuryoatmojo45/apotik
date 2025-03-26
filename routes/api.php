29c72b45f7b14d5e0cd5d5bf77d2e6fc54de53c7
Dimas Suryo A <107175975+Dimassuryoatmojo45@users.noreply.github.com>
2025-03-26 19:29:04 +0700

    Fix Transaksi

--- routes/api.php
+++ routes/api.php
@@ -28,10 +28,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('buat_apotik', [ViewController::class, 'buat_apotik']);
-    Route::get('buat_vendor', [ViewController::class, 'buat_vendor']);
-    Route::get('buat_transaksi', [ViewController::class, 'buat_transaksi'])->name('buat_transaksi');
-    Route::get('buat_transaksi_obat', [ViewController::class, 'buat_transaksi_obat'])->name('buat_transaksi_obat');
+    Route::get('apotik', [ViewController::class, 'buat_apotik']);
+    Route::get('vendor', [ViewController::class, 'buat_vendor']);
+    Route::get('transaksi', [ViewController::class, 'buat_transaksi'])->name('buat_transaksi');
+    Route::get('transaksi_obat', [ViewController::class, 'buat_transaksi_obat'])->name('buat_transaksi_obat');
     Route::get('register_admin', [ViewController::class, 'register_admin'])->name('register_admin');
     
     Route::post('/create_apotik', [ApotikController::class, 'create_apotik']);
@@ -41,4 +41,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'destroy']);
     Route::post('/register_admin', [AuthController::class, 'register_admin'])->name('register_admin');
     Route::post('/form_obat', [ViewController::class, 'form_obat'])->name('form_obat');
+    Route::post('/create_transaksi_obat', [TransaksiObatController::class, 'create_transaksi_obat'])->name('create_transaksi_obat');
 });
